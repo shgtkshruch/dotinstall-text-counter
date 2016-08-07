0f5f6aecd65b49d1f9a1167a1a2b96044df4dd38
shgtkshruch <shgtk.shruch@gmail.com>
2016-08-07 11:46:08 +0900

    Complete

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,23 @@
 (() => {
   'use strict';
 
+  const comment = document.getElementById('js-comment');
+  const label = document.getElementById('js-label');
+
+  const LIMIT = 30;
+  const WARNING = 10;
+
+  label.textContent = LIMIT;
+
+  comment.addEventListener('keyup', function(e) {
+    const remaining = LIMIT - this.value.length;
+    label.textContent = remaining;
+
+    if (remaining < WARNING) {
+      label.classList.add('textCounter__leftText--warning');
+    } else {
+      label.classList.remove('textCounter__leftText--warning');
+    }
+  }, false);
+
 })();
